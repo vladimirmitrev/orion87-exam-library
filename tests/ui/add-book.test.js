29cdc312d583f31ebfd2344c365d19479adb7f06
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-03-22 10:45:26 +0200

    Add ui test to CD pipeline

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-book');
+    await page.goto('https://orion87-exam-library.onrender.com/add-book');
     const form = await page.$('div');
     expect(form).toBeTruthy();
   });
--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/books');
+    await page.goto('https://orion87-exam-library.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
